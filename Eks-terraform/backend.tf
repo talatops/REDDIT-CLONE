efd20964d5ffde1db289bfdfc0ca139022c29b28
Talat Faheem <110402662+CYBERCODERoss@users.noreply.github.com>
2024-06-12 18:01:18 +0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "redditt"
+    bucket         = "newbucke1"
     key            = "EKS/terraform.tfstate"
     region         = "ap-south-1"
    #dynamodb_table = "terraform-lock-table"
     encrypt        = true
     #profile        = "my-aws-profile"
-    #role_arn       = "arn:aws:s3:::redditt"
+    #role_arn       = "arn:aws:s3:::newbucke1"
     #kms_key_id     = "arn:aws:kms:us-west-2:123456789012:key/your-kms-key-id"
   }
 }
